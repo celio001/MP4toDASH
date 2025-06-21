fb8c91d673d1bb65811161944b64dbb8fcaf04f4
celio001 <pessoalcelio01@gmail.com>
2025-06-21 20:09:16 -0300

    feat: adicionando o tipo job

--- MP4toDASH/domain/job.go
+++ MP4toDASH/domain/job.go
@@ -1,8 +1,13 @@
 package domain
 
+import "time"
+
 type Job struct {
 	ID              string
 	OutputBackePath string
 	Status          string
 	Video           *Video
+	Error           string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
 }
